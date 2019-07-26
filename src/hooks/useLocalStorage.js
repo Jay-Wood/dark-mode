b82a0270a43ac7e1ca180f49afd224c48cf58fb6
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-07-25 19:52:05 -0700

    Reached MVP--darkMode toggle works

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,18 +1,20 @@
 import { useState } from "react";
 
 const useLocalStorage = (key, initialValue) => {
+
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
         // Parse and return stored json or, if undefined, return initialValue
-        return item ? JSON.parse(item) : initialValue;
-    })
-    
+        return  item ? JSON.parse(item) : initialValue;
+    });
+
     const setValue = value => {
         setStoredValue(value)
         window.localStorage.setItem(key, JSON.stringify(value));        
-    }
+    };
 
-//why brackets around stored value?
-    return [storedValue];
+    return [storedValue, setValue];
 
 }
+
+export default useLocalStorage;
